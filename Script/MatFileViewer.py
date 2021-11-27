ec27d075d093522f67d2a20d9bd7022ac300d486
worlddatong <3014994021@qq.com>
2021-11-27 17:27:45 +0800

    Add files via upload

--- Script/MatFileViewer.py
+++ Script/MatFileViewer.py
@@ -7,11 +7,11 @@
 ## PLEASE DO *NOT* EDIT THIS FILE!
 ###########################################################################
 
-import os
 import wx
 import wx.xrc
 import wx.grid
 import scipy.io as sio
+
 ###########################################################################
 ## Class MyFrame1
 ###########################################################################
@@ -19,7 +19,7 @@ import scipy.io as sio
 class MyFrame1 ( wx.Frame ):
 
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"MAT File Viewer", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Mat File Viewer", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
         self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_INACTIVEBORDER ) )
@@ -39,6 +39,11 @@ class MyFrame1 ( wx.Frame ):
         self.m_grid1.EnableDragColMove( False )
         self.m_grid1.EnableDragColSize( True )
         self.m_grid1.SetColLabelSize( 30 )
+        self.m_grid1.SetColLabelValue( 0, u"1" )
+        self.m_grid1.SetColLabelValue( 1, u"2" )
+        self.m_grid1.SetColLabelValue( 2, u"3" )
+        self.m_grid1.SetColLabelValue( 3, u"4" )
+        self.m_grid1.SetColLabelValue( 4, u"5" )
         self.m_grid1.SetColLabelAlignment( wx.ALIGN_CENTER, wx.ALIGN_CENTER )
 
         # Rows
@@ -52,8 +57,23 @@ class MyFrame1 ( wx.Frame ):
         self.m_grid1.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
         bSizer1.Add( self.m_grid1, 1, wx.ALL|wx.EXPAND, 5 )
 
-        self.m_button2 = wx.Button( self, wx.ID_ANY, u"Load MAT", wx.DefaultPosition, wx.DefaultSize, 0 )
-        bSizer1.Add( self.m_button2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+        bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_button2 = wx.Button( self, wx.ID_ANY, u"Load Mat", wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer2.Add( self.m_button2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
+        self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"Variable:", wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText1.Wrap( -1 )
+
+        bSizer2.Add( self.m_staticText1, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
+        m_choice1Choices = []
+        self.m_choice1 = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, m_choice1Choices, 0 )
+        self.m_choice1.SetSelection( 0 )
+        bSizer2.Add( self.m_choice1, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
+
+        bSizer1.Add( bSizer2, 0, wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
 
         self.SetSizer( bSizer1 )
@@ -63,18 +83,42 @@ class MyFrame1 ( wx.Frame ):
 
         # Connect Events
         self.m_button2.Bind( wx.EVT_BUTTON, self.load_mat )
-
+        self.m_choice1.Bind(wx.EVT_CHOICE,self.fill_grid)
     def __del__( self ):
         pass
 
+
     def get_variable_name(self,mat_dict: dict):
-        variable_name=None
-        for i in mat_dict.keys():
-            if i.startswith('__'):
+        variable_ls=[]
+        for vari in mat_dict.keys():
+            if vari.startswith('__'):
                 pass
             else:
-                variable_name=i
-        return variable_name
+                variable_ls.append(vari)
+        return variable_ls
+    
+    def fill_grid(self,event=None):
+        sel_num=self.m_choice1.GetCurrentSelection()
+        sel_variname=self.m_choice1.GetString(sel_num)
+        data_arr=self.mat_dict[sel_variname]
+        # delete current grid
+        curcol_num=self.m_grid1.GetNumberCols()
+        self.m_grid1.DeleteCols(pos=0,numCols=curcol_num)
+        currow_num=self.m_grid1.GetNumberRows()
+        self.m_grid1.DeleteRows(pos=0,numRows=currow_num)
+        # new a grid
+        row1_num,col1_num=data_arr.shape[0],data_arr.shape[1]
+        self.m_grid1.AppendCols(col1_num)
+        self.m_grid1.AppendRows(row1_num)
+        # set values in grid
+        for i in range(row1_num):
+            for j in range(col1_num):
+                self.m_grid1.SetCellValue(i,j,s="{}".format(data_arr[i,j]))
+                
+        # set columns labels
+        for j in range(col1_num):
+                self.m_grid1.SetColLabelValue(j,"{}".format(j+1))
+        
 
     # Virtual event handlers, overide them in your derived class
     def load_mat( self, event ):
@@ -84,29 +128,16 @@ class MyFrame1 ( wx.Frame ):
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 return 
             pathname = fileDialog.GetPath()
-            mat_dict=sio.loadmat(pathname)
-            data_name=self.get_variable_name(mat_dict)
-            data_arr=mat_dict[data_name]
-            # delete current grid
-            curcol_num=self.m_grid1.GetNumberCols()
-            self.m_grid1.DeleteCols(pos=0,numCols=curcol_num)
-            currow_num=self.m_grid1.GetNumberRows()
-            self.m_grid1.DeleteRows(pos=0,numRows=currow_num)
-            # new a grid
-            row1_num,col1_num=data_arr.shape[0],data_arr.shape[1]
-            self.m_grid1.AppendCols(col1_num)
-            self.m_grid1.AppendRows(row1_num)
-            # set values in grid
-            for i in range(row1_num):
-                for j in range(col1_num):
-                    self.m_grid1.SetCellValue(i,j,s="{}".format(data_arr[i,j]))
-                    
-            # set columns labels
-            for j in range(col1_num):
-                self.m_grid1.SetColLabelValue(j,"{}".format(j+1))
+            self.mat_dict=sio.loadmat(pathname)
+            vari_ls=self.get_variable_name(self.mat_dict)
+            if len(vari_ls)>0:
+                self.m_choice1.SetItems(vari_ls)
+                self.m_choice1.SetSelection(0)
+                self.fill_grid()
+            
             
             # set frame title
-            self.SetTitle('MAT File Viewer-{}'.format(pathname))
+            self.SetTitle('Mat File Viewer-{}'.format(pathname))
         event.Skip()
 
 
